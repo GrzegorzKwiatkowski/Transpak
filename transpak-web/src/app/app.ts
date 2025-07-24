de4b48875d40c3a6294e8b8941f9f41f5856e640
GrzegorzKwiatkowski <Beo.kwiatkowski@gmail.com>
2025-07-24 16:32:43 +0200

    publish

--- transpak-web/src/app/app.ts
+++ transpak-web/src/app/app.ts
@@ -2,11 +2,14 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./header/header";
 import { Footer } from "./footer/footer";
-
+import { EuFunding } from "./eu-funding/eu-funding";
+import { Home } from './home/home';
+import { Body } from './body/body';
+import { Contact } from './contact/contact';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer],
+  imports: [Header, Footer, EuFunding, Home, Body, Contact],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
